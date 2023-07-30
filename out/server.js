36103ce90613f734201384a6a52fe509c4517ca5
Rajasegar Chandran <rajasegar.c@gmail.com>
2023-07-30 17:29:46 +0530

    chore: add hover support

--- out/server.js
+++ out/server.js
@@ -18,6 +18,7 @@ connection.onInitialize(() => {
             completionProvider: {
                 resolveProvider: true,
             },
+            hoverProvider: true,
         },
     };
 });
@@ -40,11 +41,64 @@ connection.onCompletion((_textDocumentPosition) => {
             kind: node_1.CompletionItemKind.Text,
             data: 1,
             documentation: {
-                kind: 'markdown',
+                kind: node_1.MarkupKind.Markdown,
                 value: attr.documentation,
             },
         };
     });
+});
+function findAttributeDocumentation(attributeName) {
+    const attribute = attributes_1.default.find((attr) => attr.name === attributeName);
+    return attribute ? attribute.documentation : undefined;
+}
+function getWordAtPosition(documentText, position) {
+    const offset = documentText.indexOf(documentText[documentText.length * position.line + position.character]);
+    if (offset === -1) {
+        return null;
+    }
+    let start = offset;
+    let end = offset;
+    // Move the start index to the beginning of the word
+    while (start > 0 && /\w/.test(documentText[start - 1])) {
+        start--;
+    }
+    // Move the end index to the end of the word
+    while (end < documentText.length - 1 && /\w/.test(documentText[end + 1])) {
+        end++;
+    }
+    return documentText.substring(start, end + 1);
+}
+connection.onHover((_textDocumentPosition) => {
+    const document = documents.get(_textDocumentPosition.textDocument.uri);
+    if (!document) {
+        return null;
+    }
+    const documentText = document.getText();
+    const currentWord = getWordAtPosition(documentText, _textDocumentPosition.position);
+    if (!currentWord) {
+        return null;
+    }
+    // Check if the current word is a valid HTML attribute
+    const isHtmlAttribute = attributes_1.default.some((attr) => attr.name === currentWord);
+    if (!isHtmlAttribute) {
+        return null;
+    }
+    // Find the documentation for the current attribute
+    const documentation = findAttributeDocumentation(currentWord);
+    if (!documentation) {
+        return null;
+    }
+    // Prepare the hover response
+    return {
+        contents: {
+            kind: node_1.MarkupKind.Markdown,
+            value: documentation,
+        },
+        range: {
+            start: _textDocumentPosition.position,
+            end: node_1.Position.create(_textDocumentPosition.position.line, _textDocumentPosition.position.character + currentWord.length),
+        }
+    };
 });
 connection.onCompletionResolve((item) => {
     // If you need to further customize the completion item, you can do it here.
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,9 @@ import {
   CompletionItemKind,
   TextDocumentSyncKind,
   DidChangeConfigurationNotification,
+  MarkupKind,
+  Position,
+  Hover,
 } from 'vscode-languageserver/node';
 import { TextDocument } from "vscode-languageserver-textdocument";
 import htmlAttributes from './attributes';
@@ -31,6 +34,7 @@ connection.onInitialize(() => {
       completionProvider: {
         resolveProvider: true,
       },
+      hoverProvider: true,
     },
   };
 });
@@ -59,12 +63,80 @@ connection.onCompletion((_textDocumentPosition) => {
       kind: CompletionItemKind.Text,
       data: 1,
       documentation: {
-        kind: 'markdown', // 'plaintext' is also supported
+        kind: MarkupKind.Markdown, // 'plaintext' is also supported
         value: attr.documentation,
       },
     } as CompletionItem;
   });
 });
+
+function findAttributeDocumentation(attributeName: string): string | undefined {
+  const attribute = htmlAttributes.find((attr) => attr.name === attributeName);
+  return attribute ? attribute.documentation : undefined;
+}
+
+function getWordAtPosition(documentText: string, position: Position): string | null {
+  const offset = documentText.indexOf(documentText[documentText.length * position.line + position.character]);
+  if (offset === -1) {
+    return null;
+  }
+
+  let start = offset;
+  let end = offset;
+
+  // Move the start index to the beginning of the word
+  while (start > 0 && /\w/.test(documentText[start - 1])) {
+    start--;
+  }
+
+  // Move the end index to the end of the word
+  while (end < documentText.length - 1 && /\w/.test(documentText[end + 1])) {
+    end++;
+  }
+
+  return documentText.substring(start, end + 1);
+}
+
+
+connection.onHover((_textDocumentPosition) => {
+  const document = documents.get(_textDocumentPosition.textDocument.uri);
+  if (!document) {
+    return null;
+  }
+
+  const documentText = document.getText();
+  const currentWord = getWordAtPosition(documentText, _textDocumentPosition.position);
+  if (!currentWord) {
+    return null;
+  }
+
+  // Check if the current word is a valid HTML attribute
+  const isHtmlAttribute = htmlAttributes.some((attr) => attr.name === currentWord);
+  if (!isHtmlAttribute) {
+    return null;
+  }
+
+  // Find the documentation for the current attribute
+  const documentation = findAttributeDocumentation(currentWord);
+  if (!documentation) {
+    return null;
+  }
+
+  // Prepare the hover response
+  return {
+    contents: {
+      kind: MarkupKind.Markdown,
+      value: documentation,
+    },
+    range: {
+      start: _textDocumentPosition.position,
+      end: Position.create(_textDocumentPosition.position.line, _textDocumentPosition.position.character + currentWord.length),
+    }
+  } as Hover;
+});
+
+
+
 
 
 
